3df6929e648fadb5e80cc6f841aad6557ca3b830
kushedow <Polemika2020>
2021-11-12 23:33:54 +0300

    stated defined

--- bot.py
+++ bot.py
@@ -1,42 +1,20 @@
-import requests
-import json
-
-from aiogram import Bot, types
+from aiogram import Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-from sqlrunner import SQLRunner
-
 from handlers.common import register_handlers_common
 from handlers.exercise import register_handlers_exercise
-
-
-TOKEN = "2135086897:AAGV7_YafBfAjSSYcwRhqnEvFzTCNJuKf-o"
+from config import TOKEN
 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+### Регистируем обработчики
+
 register_handlers_common(dp)
 register_handlers_exercise(dp)
-
-
-
-
-
-
-# @dp.message_handler()
-# async def echo_message(message: types.Message):
-#
-#     query = message.text
-#
-#     dumpstring = requests.get("https://raw.githubusercontent.com/learn-sql-bot/learn-sql-bot/main/levels/level_1/content/task_1/data.sql").text
-#     sqlrunner = SQLRunner()
-#     sqlrunner.install_dump(dumpstring)
-#     response = sqlrunner.run_query(query)
-#     await message.reply(str(response))
-
 
 
 if __name__ == '__main__':
--- handlers/common.py
+++ handlers/common.py
@@ -24,7 +24,7 @@ async def cmd_test(message: types.Message, state: FSMContext):
 
 
 def register_handlers_common(dp: Dispatcher):
-    dp.register_message_handler(cmd_start, commands="start", state="*")
+    dp.register_message_handler(cmd_start, commands="start", state=ExerciseState)
     dp.register_message_handler(cmd_test, commands="test", state="*")
 
     # dp.register_message_handler(cmd_cancel, commands="cancel", state="*")
